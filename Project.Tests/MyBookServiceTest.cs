a0f19094fe43f227c9d17c66a480cc35788916b1
User <User@DESKTOP-KU87SNL>
2021-03-20 16:36:18 +0200

    new test

--- Project.Tests/MyBookServiceTest.cs
+++ Project.Tests/MyBookServiceTest.cs
@@ -54,41 +54,68 @@ namespace Project.Tests
             var excpectedResult = data.Where(mb => mb.Id == myBookId).FirstOrDefault();
             Assert.AreEqual(excpectedResult.UserId, bookFound.UserId);
         }
-        //[Test]
-        //public void GetAll()
-        //{
-        //    var data = new List<MyBook>
-        //        {
-        //          new MyBook{ Id = 1,BookId =1,UserId=1},
-        //           new MyBook{ Id = 2,BookId =2,UserId=2},
-        //            new MyBook{ Id = 3,BookId =3,UserId=3},
-        //        }.AsQueryable();
+        [Test]
+        public void GetAll()
+        {
+            var data = new List<MyBook>
+                {
+                  new MyBook{ Id = 1,BookId =1,UserId=1},
+                   new MyBook{ Id = 2,BookId =2,UserId=2},
+                    new MyBook{ Id = 3,BookId =3,UserId=3},
+                }.AsQueryable();
 
-        //    var mockSet = new Mock<DbSet<MyBook>>();
-        //    mockSet.As<IQueryable<MyBook>>().Setup(m => m.Provider).Returns(data.Provider);
-        //    mockSet.As<IQueryable<MyBook>>().Setup(m => m.Expression).Returns(data.Expression);
-        //    mockSet.As<IQueryable<MyBook>>().Setup(m => m.ElementType).Returns(data.ElementType);
-        //    mockSet.As<IQueryable<MyBook>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            var mockSet = new Mock<DbSet<MyBook>>();
+            mockSet.As<IQueryable<MyBook>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<MyBook>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<MyBook>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<MyBook>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
 
-        //    var mockContext = new Mock<BookContext>();
-        //    mockContext.Setup(m => m.MyBooks).Returns(mockSet.Object);
+            var mockContext = new Mock<BookContext>();
+            mockContext.Setup(m => m.MyBooks).Returns(mockSet.Object);
 
-        //    var service = new MyBookService(mockContext.Object);
-            
-        //    var myBooks = service.GetAll(1);
-        //    var excpectedResults = data.ToList();
-        //   Assert.AreEqual(excpectedResults.Count, myBooks.Count);
-        //    for (int i = 0; i < excpectedResults.Count; i++)
-        //    {
-        //        var excpectedResult = excpectedResults[i];
-        //        var bookFound = myBooks[i];
-        //        Assert.AreEqual(excpectedResult.Id, bookFound.Id);
-        //        Assert.AreEqual(excpectedResult.UserId, bookFound.UserId);
-        //        Assert.AreEqual(excpectedResult.BookId, bookFound.BookId);
+            var service = new MyBookService(mockContext.Object);
+            var userId = 1;
+            var myBooks = service.GetAll(userId);
+            var excpectedResults = data.Where(mb => mb.UserId == userId).ToList();
+            Assert.AreEqual(excpectedResults.Count, myBooks.Count);
+            for (int i = 0; i < excpectedResults.Count; i++)
+            {
+                var excpectedResult = excpectedResults[i];
+                var bookFound = myBooks[i];
+                Assert.AreEqual(excpectedResult.Id, bookFound.Id);
+                Assert.AreEqual(excpectedResult.UserId, bookFound.UserId);
+                Assert.AreEqual(excpectedResult.BookId, bookFound.BookId);
 
 
-        //    }
+            }
+            //}
+            //    [Test]
+            //    public void Delete()
+            //    {
+            //        var data = new List<MyBook>
+            //        {
+            //          new MyBook{ Id = 1,BookId =1,UserId=1},
+            //           new MyBook{ Id = 2,BookId =2,UserId=2},
+            //            new MyBook{ Id = 3,BookId =3,UserId=3},
+            //        }.AsQueryable();
+
+            //        var mockSet = new Mock<DbSet<MyBook>>();
+            //        mockSet.As<IQueryable<MyBook>>().Setup(m => m.Provider).Returns(data.Provider);
+            //        mockSet.As<IQueryable<MyBook>>().Setup(m => m.Expression).Returns(data.Expression);
+            //        mockSet.As<IQueryable<MyBook>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            //        mockSet.As<IQueryable<MyBook>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            //        var mockContext = new Mock<BookContext>();
+            //        mockContext.Setup(m => m.MyBooks).Returns(mockSet.Object);
+
+            //        var service = new MyBookService(mockContext.Object);
+            //        var myBookId = 1;
+            //         service.Delete(myBookId);
+
+            //}
         }
     }
+}
+
 
 
